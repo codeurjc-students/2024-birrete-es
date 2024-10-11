7939799ddc28d88bb0f7c94a865c0bda89ec838c
ulises-huaman-urjc <ua.huaman@alumnos.urjc.es>
2024-10-11 20:11:36 +0200

    Personalización del header

--- front-end/src/components/header/appbar/AppBar.js
+++ front-end/src/components/header/appbar/AppBar.js
@@ -2,29 +2,44 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Logo from '../../../static-images/images-header/logo150x50.png';
+import LoginIcon from '@mui/icons-material/Login';
+import AppRegistrationSharpIcon from '@mui/icons-material/AppRegistrationSharp';
 
 const BarApp = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
+        <AppBar position="static" sx={{backgroundColor: "#FEFEFE"}}>
         <Toolbar>
             <IconButton
             size="large"
             edge="start"
-            color="inherit"
+            color="black"
             aria-label="menu"
             sx={{ mr: 2 }}
             >
             <MenuIcon />
             </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-            </Typography>
-            <Button color="inherit">Login</Button>
+            <IconButton>
+              <Box
+                component="img"              
+                src={Logo}
+                alt="Logo de Birrete-es"
+              >
+              </Box>
+            </IconButton>
+            <Box sx={{flexGrow: 1, textAlign: "right"}}>
+                <IconButton sx={{fontSize: "15px"}}>
+                    <AppRegistrationSharpIcon fontSize="small"/>
+                    Register
+                </IconButton>
+                <IconButton sx={{fontSize: "15px"}}>
+                    <LoginIcon fontSize="small"/>
+                    Loging
+                </IconButton>               
+            </Box>           
         </Toolbar>
         </AppBar>
     </Box>
